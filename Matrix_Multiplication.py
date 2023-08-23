a6a9a16c7bed4624b7a169790e04fda688fb1a77
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-08-23 14:29:17 +0600

    Create Matrix_Multiplication.py

---
+++ Matrix_Multiplication.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Create matrices
+A = np.array([[1, 2], [3, 4]])
+B = np.array([[5, 6], [7, 8]])
+
+# Matrix multiplication
+C = np.dot(A, B)
+
+print("Matrix C:")
+print(C)
+
+# Answer: Matrix C:
+#         [[19 22]
+#          [43 50]]
