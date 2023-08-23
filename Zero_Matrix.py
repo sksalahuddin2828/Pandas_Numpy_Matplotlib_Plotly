a6bc1574318051c626ec4694ab69ff8f5b086074
SK. SALAHUDDIN <127140921+sksalahuddin2828@users.noreply.github.com>
2023-08-23 14:22:51 +0600

    Create Zero_Matrix.py

---
+++ Zero_Matrix.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Create a 2x2 zero matrix
+zero_matrix = np.zeros((2, 2))
+
+print("Zero Matrix:")
+print(zero_matrix)
+
+# Answer: Zero Matrix:
+#         [[0. 0.]
+#          [0. 0.]]
